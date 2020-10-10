54a2f4a7f41e65011393226c20f1954116d271d0
shobhit09 <shobhitkumar@deloitte.com>
2020-10-10 14:38:53 +0530

    Made changes to show Volcanoes marker and population polygon on map

--- WebMap/Main.py
+++ WebMap/Main.py
@@ -1,7 +1,7 @@
 # @author - Shobhit 
 
-''' folium is a library in python which is used to create base map by converting the parameters passed
- into javascript and HTML code. It uses Leaflet library'''
+# folium is a library in python, used to create base map by converting the parameters
+#into javascript and HTML code. It uses Leaflet library
 import folium
 import pandas
 
@@ -23,12 +23,21 @@ def getColor(inp):
 map = folium.Map(location=[39.291681, -94.651297], zoom_start=6, tiles = "Stamen Terrain")
 
 # creating a feature group, feature group helps in enabling/disabling layers of map
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 # creating  markers on the map
 
 for lat,lon,elev in zip(latitude,longitude,elevation):
     
-    fg.add_child(folium.Marker(location=[lat, lon], popup=elev, icon=(folium.Icon(color=getColor(elev)))))
+    fgv.add_child(folium.Marker(location=[lat, lon], popup=elev, icon=(folium.Icon(color=getColor(elev)))))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+# GeoJson adds polygon layer to the map and based on population we are assoigning different layers to the polygon
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'red' if x['properties']['POP2005'] > 20000000 else 'yellow'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+# adding layer control
+map.add_child(folium.LayerControl())
 map.save("baseMap.html")
